537d673f5d92d223f0f42b3c47d4f4b4a8f017c9
mickers7 <55769169+mickers7@users.noreply.github.com>
2019-09-24 15:57:12 -0700

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,34 @@
+player = input("Enter r,p,s: ")
+
+from random import *
+x = randint(1, 3)
+
+if player == ("r"):
+    print ("you picked rock")
+    if (x) == (1):
+        print ("Tie")
+    elif (x) == (2):
+        print ("You Lose")
+    elif (x) == (3):
+        print ("You Win!")
+
+elif player == ("p"):
+    print ("you picked paper")
+    if (x) == (1):
+        print ("You Win!")
+    elif (x) == (2):
+        print ("Tie")
+    elif (x) == (3):
+        print ("You Lose")
+
+elif player == ("s"):
+    print ("you picked scissors")
+    if (x) == (1):
+        print ("You Lose")
+    elif (x) == (2):
+        print ("You Win!")
+    elif (x) == (3):
+        print ("Tie")
+else:
+    print ("please try again")
+
